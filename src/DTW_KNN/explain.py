d8a6906c0c8a7a88b28d71374cfbb7f99faba17b
Jan-Eike <j-e_brocker@t-online.de>
2021-06-22 16:23:19 +0200

    added more stuff for plotting and fixed a bug

--- src/DTW_KNN/explain.py
+++ src/DTW_KNN/explain.py
@@ -10,16 +10,22 @@ def main():
     channel, k_nearest_time_series, best_paths  = load_classification_data()
     labvitals_list_test = load_current_test_data()
     plot_explain(k_nearest_time_series, labvitals_list_test, best_paths, channel[1])
+    plot_all_channels(k_nearest_time_series, labvitals_list_test, best_paths, channel)
 
 
 def plot_explain(k_nearest_time_series, labvitals_list_test, best_paths, channel):
-    # plot for test point 0
-    time_series_1 = np.array(k_nearest_time_series[0][0].iloc[:, 6:].iloc[:, [channel]], dtype='float64').reshape(-1,)
-    time_series_2 = np.array(labvitals_list_test[0].iloc[:, 6:].iloc[:, [channel]], dtype='float64').reshape(-1,)
-    # the first 0 stands for the current test point when this will be done automatically in the end
-    print(k_nearest_time_series[0][0].iloc[:, 6:].iloc[:, [channel]])
-    print(labvitals_list_test[0].iloc[:, 6:].iloc[:, [channel]])
-    dtwvis.plot_warping(time_series_2, time_series_1, best_paths[0][0])
+    # the first indexing number stands for the current test point when this will be done automatically in the end (or not idk)
+    test_point = 2
+    time_series_1 = np.array(k_nearest_time_series[test_point][0].iloc[:, 6:].iloc[:, [channel]], dtype='float64').reshape(-1,)
+    time_series_2 = np.array(labvitals_list_test[test_point].iloc[:, 6:].iloc[:, [channel]], dtype='float64').reshape(-1,)
+    print(k_nearest_time_series[test_point][0].iloc[:, 6:].iloc[:, [channel]])
+    print(labvitals_list_test[test_point].iloc[:, 6:].iloc[:, [channel]])
+    fig, ax = dtwvis.plot_warping(time_series_2, time_series_1, best_paths[2][0])
+    fig.set_size_inches(15, 8)
+    fig.subplots_adjust(hspace=0.2)
+    fig.suptitle(labvitals_list_test[test_point].iloc[:, 6:].iloc[:, [channel]].columns[0], y=1)
+    ax[0].set_title("test data time series", y=1, pad=-14)
+    ax[1].set_title("nearest neighbor", y=1, pad=-14)
     plt.show()
 
 
--- src/DTW_KNN/find_hyperparameter.py
+++ src/DTW_KNN/find_hyperparameter.py
@@ -21,9 +21,9 @@ def find_best_k(labvitals_time_series_list_val, labels_val, k_list, save=True):
     scores_dataframe = pd.DataFrame(columns=["k", "average acc score", "average auprc score"])
     avg_scores = []
     for k in tqdm(k_list, desc="finding best k"):
-        avg_acc_score, avg_auprc_score = cross_validate(labvitals_time_series_list_val, labels_val, k)
+        avg_auprc_score, avg_roc_auc_score = cross_validate(labvitals_time_series_list_val, labels_val, k)
         # save scores in dictionary for each k
-        scores_dict = {"k" : k, "average acc score" : avg_acc_score, "average auprc score" : avg_auprc_score}
+        scores_dict = {"k" : k, "average auprc score" : avg_auprc_score, "average roc auc score" : avg_roc_auc_score}
         scores_dataframe = scores_dataframe.append(scores_dict, ignore_index=True)
         avg_scores.append(avg_auprc_score)
         if save:
@@ -45,10 +45,10 @@ def cross_validate(labvitals_time_series_list_val, labels_val, k):
         k (integer): parameter k that gets cross validated
 
     Returns:
+            float: mean auprc_score of all scores achieved during cross validation  
         float: mean roc_auc_score of all scores achieved during cross validation
-        float: mean auprc_score of all scores achieved during cross validation
     """
-    splits = 4
+    splits = 3
     kf = KFold(n_splits=splits, random_state=42, shuffle=True)
     scores_auprc = []
     scores_roc_auc = []
@@ -59,9 +59,9 @@ def cross_validate(labvitals_time_series_list_val, labels_val, k):
         X_test = index_time_series_list(labvitals_time_series_list_val, test_index)
         y_train , y_test = labels_val[train_index] , labels_val[test_index]
         #dtw_matrices_train, dtw_matrices_test = calculate_distance_matrices(X_train, X_test, len(X_train), len(X_test))
-        mean_score_roc_auc, mean_score_auprc = classify(X_train, X_test, y_train, y_test, best_k=k, print_res=False)
+        mean_score_auprc, mean_score_roc_auc= classify(X_train, X_test, y_train, y_test, best_k=k, print_res=False)
         scores_auprc.append(mean_score_auprc)
         scores_roc_auc.append(mean_score_roc_auc)
         pbar.update(1)
     pbar.close()
-    return np.mean(scores_roc_auc), np.mean(scores_auprc)
+    return np.mean(scores_auprc), np.mean(scores_roc_auc)
